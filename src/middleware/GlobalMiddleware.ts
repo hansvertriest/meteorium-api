3502eef3c516852855bb582f7af2b6e31a9a5857
Hans Vertriest <hans.vertriest@gmail.com>
2022-10-23 14:00:13 +0200

    :wrench: added morgan and cors middleware

--- src/middleware/GlobalMiddleware.ts
+++ src/middleware/GlobalMiddleware.ts
@@ -1,16 +1,19 @@
 // Node
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 export default class GlobalMiddleware {
-    private app: express.Application;
+  private app: express.Application;
 
-    constructor( app: express.Application ) {
-        this.app = app;
-    }
+  constructor(app: express.Application) {
+    this.app = app;
+  }
 
-    public load = () => {
-        this.app.use(express.json());
-        this.app.use(express.urlencoded());
-    }
-
+  public load = () => {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded());
+    this.app.use(morgan());
+    this.app.use(cors());
+  };
 }
